a42847d6cb92e29f1a2fca6070093921b55a082c
vern-gora <103502360+vern-gora@users.noreply.github.com>
2023-09-15 01:16:53 +0300

    fix

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,3 @@
 export const selectContacts = state => state.contacts.items;
-export const selectFilter = state => state.filter.value;
+export const selectFilter = state => state.filter;
 export const selectIsLoading = state => state.contacts.isLoading;
--- src/redux/slices.js
+++ src/redux/slices.js
@@ -1,14 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { addContact, deleteContact, fetchContacts } from './operations';
 
-const handlePending = state => {
-  state.isLoading = true;
-};
+// const handlePending = state => {
+//   state.isLoading = true;
+// };
 
-const handleRejected = (state, action) => {
-  state.isLoading = false;
-  state.error = action.payload;
-};
+// const handleRejected = (state, action) => {
+//   state.isLoading = false;
+//   state.error = action.payload;
+// };
 
 export const addContactSlice = createSlice({
   name: 'contacts',
@@ -17,38 +17,74 @@ export const addContactSlice = createSlice({
     isLoading: false,
     error: null,
   },
-  extraReducers: {
-    [fetchContacts.pending]: handlePending,
-    [fetchContacts.fulfilled]: (state, action) => {
-      state.items = action.payload;
-      state.isLoading = false;
-    },
-    [fetchContacts.rejected]: handleRejected,
-    [deleteContact.pending]: handlePending,
-    [deleteContact.fulfilled]: (state, action) => {
-      state.isLoading = false;
-      state.items = state.items.filter(
-        contact => contact.id !== action.payload.id
-      );
-    },
-    [deleteContact.rejected]: handleRejected,
-    [addContact.pending]: handlePending,
-    [addContact.fulfilled]: (state, action) => {
-      state.items.unshift(action.payload);
-      state.isLoading = false;
-    },
-    [addContact.rejected]: handleRejected,
+  reducers: {},
+  extraReducers: builder => {
+    builder
+      .addCase(fetchContacts.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        state.items = action.payload;
+        state.isLoading = false;
+      })
+      .addCase(fetchContacts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+      .addCase(deleteContact.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(deleteContact.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.items = state.items.filter(
+          contact => contact.id !== action.payload.id
+        );
+      })
+      .addCase(deleteContact.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+      .addCase(addContact.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(addContact.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.items.unshift(action.payload);
+      })
+      .addCase(addContact.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      });
+    //   extraReducers: {
+    //     [fetchContacts.pending]: handlePending,
+    //     [fetchContacts.fulfilled]: (state, action) => {
+    //       state.items = action.payload;
+    //       state.isLoading = false;
+    //     },
+    //     [fetchContacts.rejected]: handleRejected,
+    //     [deleteContact.pending]: handlePending,
+    //     [deleteContact.fulfilled]: (state, action) => {
+    //       state.isLoading = false;
+    //       state.items = state.items.filter(
+    //         contact => contact.id !== action.payload.id
+    //       );
+    //     },
+    //     [deleteContact.rejected]: handleRejected,
+    //     [addContact.pending]: handlePending,
+    //     [addContact.fulfilled]: (state, action) => {
+    //       state.items.unshift(action.payload);
+    //       state.isLoading = false;
+    //     },
+    //     [addContact.rejected]: handleRejected,
   },
 });
 
 export const filterSlice = createSlice({
   name: 'filter',
-  initialState: {
-    value: '',
-  },
+  initialState: '',
   reducers: {
     filter: (state, action) => {
-      state.value = action.payload;
+      return (state = action.payload);
     },
   },
 });
